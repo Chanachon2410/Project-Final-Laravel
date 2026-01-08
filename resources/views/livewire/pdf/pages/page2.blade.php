f664dc3aa1348c89b9e05156f737adebbf462849
Chanachon2410 <nmzyingtuu24@gmail.com>
2026-01-08 16:52:36 +0700

    8/1/69

--- resources/views/livewire/pdf/pages/page2.blade.php
+++ resources/views/livewire/pdf/pages/page2.blade.php
@@ -19,26 +19,29 @@
         height: 16px;
         border: 1px solid #000;
         text-align: center;
-        line-height: 16px;
+        line-height: 14px;
         margin-right: -1px;
         font-family: monospace;
         font-size: 10pt;
         font-weight: bold;
+        vertical-align: middle; /* แก้ปัญหาช่องว่างกับช่องมีเลขไม่เท่ากัน */
     }
 </style>
 
 <div class="pdf-page page2-container">
     @for($part = 1; $part <= 2; $part++)
         <div style="border: 1px solid #000; padding: 4mm; position: relative; box-sizing: border-box; margin-bottom: 2mm;">
-            <table class="w-full" style="border: none;">
+            <table class="w-full" style="border: none; margin-bottom: 15px;">
                 <tr style="border: none;">
-                    <td style="width: 60px; vertical-align: top; border: none;">
-                        <img src="{{ ($isPdf ?? false) ? public_path('images/logo (1).png') : asset('images/logo (1).png') }}" alt="Logo" style="width: 60px; height: auto;">
+                    <td style="width: 80px; vertical-align: top; border: none;">
+                        <img src="{{ ($isPdf ?? false) ? public_path('images/logo (1).png') : asset('images/logo (1).png') }}" alt="Logo" style="width: 80px; height: auto;">
                     </td>
                     <td class="text-center" style="border: none; padding-left: 0mm;">
-                        <div style="font-size: 14pt; font-weight: 300;">ใบแจ้งการชำระเงินค่าบำรุงการศึกษา</div>
-                        <div style="font-size: 12pt; font-weight: 300;">วิทยาลัยอาชีวศึกษานครปฐม</div>
-                        <div style="font-size: 10pt; font-weight: 300;">Company Code : 81245</div>
+                        <div style="font-size: 14pt; font-weight: 300; margin-bottom: 5px;">ใบแจ้งการชำระเงินค่าบำรุงการศึกษา</div>
+                        <div style="font-size: 12pt; font-weight: 300; margin-bottom: 5px;">วิทยาลัยอาชีวศึกษานครปฐม</div>
+                        @if($part == 1)
+                            <div style="font-size: 10pt; font-weight: 300;">Company Code : 81245</div>
+                        @endif
                     </td>
                     <td class="text-right" style="vertical-align: top; width: 180px; border: none; padding-right: 5mm;">
                         <div class="font-bold" style="font-size: 10pt;">ส่วนที่ {{ $part }} (สำหรับ{{ $part == 1 ? 'นักเรียน' : 'ธนาคาร' }})</div>
@@ -47,32 +50,43 @@
                 </tr>
             </table>
 
-            <div style="margin: 5px 0; font-size: 9pt;">ชื่อ - นามสกุลนักเรียน : {{ $student_name ?? '................................................................................................' }}</div>
-            
-            <div style="margin-bottom: 5px; font-size: 9pt;">
-                รหัสประจำตัวสอบ (Ref.1) : 
-                <div class="page2-ref-container">
-                    @php $s_code = str_pad($student_code ?? '', 13, ' ', STR_PAD_RIGHT); @endphp
-                    @for($i=0; $i<13; $i++)
-                        <div class="page2-ref-box">{{ trim(substr($s_code, $i, 1)) }}</div>
-                    @endfor
-                </div>
-            </div>
-
-            <div style="margin-bottom: 5px; font-size: 9pt;">
-                รหัสกลุ่ม (Ref.2) : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <div class="page2-ref-container">
-                    @php $g_code = str_pad($group_code ?? '', 6, ' ', STR_PAD_RIGHT); @endphp
-                    @for($i=0; $i<6; $i++)
-                        <div class="page2-ref-box">{{ trim(substr($g_code, $i, 1)) }}</div>
-                    @endfor
-                </div>
-            </div>
-
             @if($part == 1)
-            <div style="border: 1px solid #000; font-size: 8pt; background-color: #fafafa; margin-bottom: 5px; padding: 3px;">
-                <div class="text-center font-bold" style="text-decoration: underline; margin-bottom: 2px;">ตัวอย่างการกรอก Ref.2 ของระดับชั้น {{ $level_name ?? '...' }} ทุกแผนกวิชา</div>
-                <table class="w-full" style="border: none;">
+            {{-- ส่วนที่ 1: ข้อมูลนักเรียนแบบเดิม --}}
+            <table style="width: 100%; border: none; margin-bottom: 8px; font-size: 9pt;">
+                <tr style="border: none;">
+                    <td style="width: 130px; border: none; padding-bottom: 5px; vertical-align: middle;">ชื่อ - นามสกุลนักเรียน</td>
+                    <td style="width: 20px; border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">{{ $student_name ?? '................................................................................................' }}</td>
+                </tr>
+                <tr style="border: none;">
+                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">รหัสประจำตัวสอบ (Ref.1)</td>
+                    <td style="border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">
+                        <div class="page2-ref-container" style="height: 18px;">
+                            @php $s_code = str_pad($student_code ?? '', 13, ' ', STR_PAD_RIGHT); @endphp
+                            @for($i=0; $i<13; $i++)
+                                <div class="page2-ref-box">{{ trim(substr($s_code, $i, 1)) }}</div>
+                            @endfor
+                        </div>
+                    </td>
+                </tr>
+                <tr style="border: none;">
+                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">รหัสกลุ่ม (Ref.2)</td>
+                    <td style="border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">
+                        <div class="page2-ref-container" style="height: 18px;">
+                            @php $g_code = str_pad($group_code ?? '', 6, ' ', STR_PAD_RIGHT); @endphp
+                            @for($i=0; $i<6; $i++)
+                                <div class="page2-ref-box">{{ trim(substr($g_code, $i, 1)) }}</div>
+                            @endfor
+                        </div>
+                    </td>
+                </tr>
+            </table>
+
+            <div style="border: 1px solid #000; font-size: 9.5pt; background-color: #fafafa; margin-bottom: 5px; padding: 3px;">
+                <div class="text-center font-bold" style="text-decoration: underline; margin-bottom: 5px; font-size: 10pt;">ตัวอย่างการกรอก Ref.2 ของระดับชั้น {{ $level_name ?? '...' }} ทุกแผนกวิชา</div>
+                <table class="w-full" style="border: none; font-size: 9.5pt;">
                     @if(isset($all_majors) && count($all_majors) > 0)
                         @php
                             $chunks = $all_majors->chunk(3); // แบ่งข้อมูลทีละ 3 สาขาต่อแถว
@@ -99,10 +113,52 @@
                 </table>
             </div>
             @else
-            <div style="margin: 5px 0;">
-                <div class="font-bold" style="color: #0056b3; font-size: 12pt;">ธนาคารกรุงไทย จำกัด (มหาชน)</div>
-                <div class="font-bold">Company Code : 81245</div>
-            </div>
+            {{-- ส่วนที่ 2: ธนาคาร (แบ่งซ้าย-ขวา) --}}
+            <table style="width: 100%; border: none; margin-bottom: 5px;">
+                <tr style="border: none;">
+                    {{-- ฝั่งซ้าย: ข้อมูลธนาคาร --}}
+                    <td style="width: 40%; vertical-align: top; border: none; padding-right: 10px;">
+                        <div class="font-bold" style="color: #0056b3; font-size: 12pt; margin-bottom: 5px;">ธนาคารกรุงไทย จำกัด (มหาชน)</div>
+                        <div class="font-bold" style="font-size: 10pt;">Company Code : 81245</div>
+                    </td>
+                    {{-- ฝั่งขวา: ข้อมูลนักเรียนในกรอบ --}}
+                    <td style="width: 60%; vertical-align: top; border: none;">
+                        <div style="border: 1px solid #000; padding: 10px; border-radius: 0px;">
+                            <table style="width: 100%; border: none; font-size: 9pt;">
+                                <tr style="border: none;">
+                                    <td style="width: 110px; border: none; padding-bottom: 5px; vertical-align: middle;">ชื่อ - นามสกุลนักเรียน</td>
+                                    <td style="width: 10px; border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">{{ $student_name ?? '.........................................' }}</td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">รหัสประจำตัวสอบ (Ref.1)</td>
+                                    <td style="border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">
+                                        <div class="page2-ref-container" style="height: 18px;">
+                                            @php $s_code = str_pad($student_code ?? '', 13, ' ', STR_PAD_RIGHT); @endphp
+                                            @for($i=0; $i<13; $i++)
+                                                <div class="page2-ref-box">{{ trim(substr($s_code, $i, 1)) }}</div>
+                                            @endfor
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr style="border: none;">
+                                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">รหัสกลุ่ม (Ref.2)</td>
+                                    <td style="border: none; text-align: center; padding-bottom: 5px; vertical-align: middle;">:</td>
+                                    <td style="border: none; padding-bottom: 5px; vertical-align: middle;">
+                                        <div class="page2-ref-container" style="height: 18px;">
+                                            @php $g_code = str_pad($group_code ?? '', 6, ' ', STR_PAD_RIGHT); @endphp
+                                            @for($i=0; $i<6; $i++)
+                                                <div class="page2-ref-box">{{ trim(substr($g_code, $i, 1)) }}</div>
+                                            @endfor
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+            </table>
             @endif
 
             <table class="page2-table">
